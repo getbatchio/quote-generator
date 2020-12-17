88baa528be4d0d60c16589007eeb0249d1d44596
Luke Batchelor <getbatchio@gmail.com>
2020-12-17 18:36:35 -0500

    added https to apiUrl and added proxyUrl

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Get Quote From API
 async function getQuote() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
     console.log(data);
   } catch (error) {
